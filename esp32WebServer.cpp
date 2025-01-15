30de3ed9b0c72713cb5c91f52ef7ea73ca3155c7
Eugenethiri <eugene.thirikali@strathmore.edu>
2025-01-15 15:12:38 +0300

    Create esp32WebServer.cpp
    cant get the custom ssid+pass working:( ... yet

---
+++ esp32WebServer.cpp
@@ -0,0 +1,48 @@
+#include <WiFi.h>
+
+const char* ssid = "xxxxxxxx";
+const char* password = "xxxxx"; 
+
+WiFiServer server(80);
+
+
+void setup(){
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_AP);
+
+  Serial.print("Setting AP:");
+  WiFi.softAP(ssid, password);
+  Serial.print("AP IP address: ");
+  Serial.println(WiFi.softAPIP());
+
+  server.begin();
+  Serial.println("Server started");
+}
+
+void loop(){
+  WiFiClient client = server.available();
+
+  if (client){
+    Serial.println("new client connected");
+
+    String request = client.readString();
+    Serial.println(request);
+
+    // Send an HTTP response
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: text/html");
+    client.println("Connection: close");
+    client.println();
+    
+    // Send a simple HTML page
+    client.println("<html><body>");
+    client.println("<h1>Hello from MArs!</h1>");
+    client.println("<p>Welcome to my ESP32 Access Point.</p>");
+    client.println("</body></html>");
+
+    // Close the client connection
+    client.stop();
+    Serial.println("Client disconnected");
+  }
+}
