43db07fe5e021beb04f935b2349cdbbea9d4a80d
Eugenethiri <eugene.thirikali@strathmore.edu>
2025-02-10 11:35:22 +0300

    Update clientSniffer.cpp
    back to the drawing board :(

--- clientSniffer.cpp
+++ clientSniffer.cpp
@@ -1,32 +1,15 @@
-#include "WiFi.h"
-
-void packetSniffer(void* buf, wifi_promiscuous_pkt_type_t type){ 
-  wifi_promiscuous_pkt_t* pkt = (wifi_promiscuous_pkt_t*)buf;
-
-if (type == WIFI_PKT_MGMT){
-  Serial.println("Management frame captured"); //4 testing lets scan everything
-  Serial.print("packet data");
-
-  for (int i = 0; i< pkt->payload_len; i++){
-    Serial.print(pkt->payload[i], HEX);
-    Serial.print(" "); //some space for organisation
+void printError(esp_err_t err) {
+    switch (err) {
+        case ESP_OK:
+            Serial.println("Success.");
+            break;
+        case ESP_ERR_WIFI_NOT_INIT:
+            Serial.println("Error: WiFi not initialized.");
+            break;
+        case ESP_ERR_WIFI_NOT_STARTED:
+            Serial.println("Error: WiFi not started.");
+            break;
+        default:
+            Serial.printf("Unknown error code: %d\n", err);
     }
-    Serial.println();
-    }
 }
-
-void setup(){
-  Serial.begin(115200); //listen here to the serial monitor 
-  WiFi.mode(WIFI_STA);  //station
-  WiFi.disconnect();
-  delay(1000); //short break coz everyone needs them
-
-//Setting up the listener settings
-  esp_wifi_set_promiscuous(true);
-  wifi_promiscuous_set_rx_cb(packetSniffer); //scan specific frame (management frames only);
-}
-
-void loop(){
-  delay (1000); //short delay in ms to keep it running 
-}
-
