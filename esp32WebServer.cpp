93bb23ac5e6b42d1f2ef2ab027cb8ddcb0565505
Eugenethiri <eugene.thirikali@strathmore.edu>
2025-02-13 22:33:05 +0300

    Update esp32WebServer.cpp

--- esp32WebServer.cpp
+++ esp32WebServer.cpp
@@ -2,112 +2,61 @@
 #include <DNSServer.h>
 
 // ;LISTEN UP!!  
-const char* ssid = "xxxxxxxx"; //max is 32 characters
-const char* password = "12345678";  // this has to be 8 character else it resorts to default 
+const char* ssid = "xxxxxxxx";  // Max is 32 characters
+const char* password = "12345678";  // Must be at least 8 characters, else it defaults
 
 WiFiServer server(80);
-DNSServer dnsServer;  // Creates a DNS server instance yk domain name "no" server
+DNSServer dnsServer;  // Creates a DNS server instance
 
-void setup(){
-  Serial.begin(115200);
+void setup() {
+    Serial.begin(115200);
 
-  WiFi.mode(WIFI_AP);
-  Serial.print("Setting AP:");
-  WiFi.softAP(ssid, password);
-  Serial.print("AP IP address: ");
-  Serial.println(WiFi.softAPIP());
+    WiFi.mode(WIFI_AP);
+    Serial.print("Setting AP:");
+    WiFi.softAP(ssid, password);
+    Serial.print("AP IP address: ");
+    Serial.println(WiFi.softAPIP());
 
-  dnsServer.start(53, "*", WiFi.softAPIP()); //dns hijack to port 53, all web querrys, to the esps ip address
+    dnsServer.start(53, "*", WiFi.softAPIP());  // DNS hijack to port 53, reroutes all queries
 
-  server.begin();
-  Serial.println("Server started");
+    server.begin();
+    Serial.println("Server started");
 }
-
-void handleClient(WiFiClient client) {
-  Serial.println("New victim connected");
-  String request = client.readString();
-  Serial.println(request);
-
-  //simulation of dial request
-  if (request.indexOf("generate_204") >= 0 ||
-      request.indexOf("hotspot-detect.html") >= 0 ||
-      request.indexOf("ncsi.txt") >= 0
-      ) {
-    client.println("HTTP/1.1 302 Found");
-    client.println("Location: http://192.168.4.1/portal");
-    client.println("Content-Length: 0");
-    client.println();
-    client.stop();
-    return;
-        }
-
-  if (request.indexOf("GET /portal") >= 0 ||
-      request.indexOf("GET /") >= 0
-        ) {
-    client.println("HTTP/1.1 302 Found");
-    client.println("Location: http://192.168.4.1");
-    client.println("Content-Length: 0");
-    client.println();
-    client.stop();
-          }
-  else {
-    client.println("HTTP/1.1 302 Found");
-    client.println("Location: http://192.168.4.1");
-    client.println("Content-Length: 0");
-    client.close();
-    }
-
-void logRequest() {
-  File logFile = SPIFFS.open("/replace", "a");
-  if (!logfile) {
-    Seral.println("FAiled to open html");
-    return;
-  }
-  logFile.println(request);
-  LogFile.close();
-}
-
 
 void loop() {
     dnsServer.processNextRequest();  // Handle DNS requests
 
     WiFiClient client = server.available();
     if (client) {
-        Serial.println("New client connected");
-
+        Serial.println("New victim connected");
         String request = client.readString();
         Serial.println(request);
 
-/*
-          problem is the 302 redirection may not be followed by sone devices
-      3: TRY implementing SPIFF, i dont like how im adding the whole html on this script gotta add it to /spiff 
-      orr
-      3.5: memory card module way better 
-      speaking of memory esp to listen to music :D      
-*/
-
-        // rewrite this and do the index.html SPIFF redirection
-        if (request.indexOf("generate_204") > 0 || 
-            request.indexOf("hotspot-detect.html") > 0 || 
-            request.indexOf("ncsi.txt") > 0) {
-            client.println("HTTP/1.1 200 OK");
+        // Simulation of dial request
+        if (request.indexOf("generate_204") >= 0 ||
+            request.indexOf("hotspot-detect.html") >= 0 ||
+            request.indexOf("ncsi.txt") >= 0) {
+            client.println("HTTP/1.1 302 Found");
+            client.println("Location: http://192.168.4.1/portal");
             client.println("Content-Length: 0");
             client.println();
             client.stop();
             return;
         }
 
-        // Serve Warning Page
-        client.println("HTTP/1.1 200 OK");
-        client.println("Content-Type: text/html"); // just empty with no content 
-        client.println("Connection: close");
-        client.println("Cache-Control: no-cache, no-store, must-revalidate");
-        client.println("Pragma: no-cache");
-        client.println("Expires: 0");
-        client.println();
-
-  
-        // Security Warning Page (Your Custom HTML)
+        if (request.indexOf("GET /portal") >= 0 ||
+            request.indexOf("GET /") >= 0) {
+            client.println("HTTP/1.1 302 Found");
+            client.println("Location: http://192.168.4.1");
+            client.println("Content-Length: 0");
+            client.println();
+            client.stop();
+        } else {
+            client.println("HTTP/1.1 302 Found");
+            client.println("Location: http://192.168.4.1");
+            client.println("Content-Length: 0");
+            client.stop(); 
+        }
         client.println("<html><head><style>");
         client.println("body { font-family: Arial, sans-serif; background-color: #f4f4f4; color: #333; padding: 20px; }");
         client.println("h1 { color: red; }");
