092765437c0b701744b84964da56f6c6079a3a2b
pickleTK <ryle.thirikali@gmail.com>
2025-07-23 11:23:33 +0300

    updates

--- APscannerCpp.ino
+++ APscannerCpp.ino
@@ -4,13 +4,13 @@
 //define the auth types WiFi use
 const char* wifi_auth_types[] = {
 
-      "**[0PEN N3TWORK]**",
-      "**[WEP PROTECTION] ULTRA W3AK**",
-      "**[WPA PROTECTION]**",
-      "**[WPA2 PR0TECTI0N]**",
-      "**[WPA/WPA2 PR0TECTION]**",
-      "**[WPA2-3NTERPRISE PROTECTION]**",
-      "**[WPA3 PROTECTION]**"
+      "[0PEN]",
+      "[WEP] ULTRA W3AK",
+      "[WPA]",
+      "[WPA2]",
+      "[WPA/WPA2]",
+      "[WPA2-ENT]",
+      "[WPA3]"
       };
 
 void setup() {
@@ -22,7 +22,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   Serial.println("scan start ");
   Serial.println("   ⋆༺𓆩☠︎︎𓆪༻⋆ \n");
   
@@ -33,24 +32,23 @@ void loop() {
     Serial.println("no networks found");
   }
   else {
-    Serial.printf("𒉭 %d 𒉭networks found.\n" , n);
+    Serial.printf("𒉭%d networks found𒉭.\n " , n);
     
     for (int i = 0; i < n; i++){
-      Serial.printf("%d: %s ♱ MAC: %s ♱ AP RSSI: %d ♱ Channel: %d ♱\n",
+      uint8_t auth_type= WiFi.encryptionType(i);
+      const char* auth_type_str = (auth_type <= WIFI_AUTH_WPA3_PSK) ? wifi_auth_types[auth_type] : "**[UNKN0WN]**";
+
+      Serial.printf("%d: %s ♱MAC: %s ♱RSSI: %d ♱CH: %d ♱Auth: %s ♱ \n",
                     i + 1,
                     WiFi.SSID(i).c_str(),
                     WiFi.BSSIDstr(i).c_str(),
                     WiFi.RSSI(i),
-                    WiFi.channel(i));
-      
-      uint8_t auth_type= WiFi.encryptionType(i);
-      const char* auth_type_str = (auth_type <= WIFI_AUTH_WPA3_PSK) ? wifi_auth_types[auth_type] : "**[UNKN0WN]**";
-      Serial.println(auth_type_str);
-      
+                    WiFi.channel(i),
+                    auth_type_str);
       delay(10);
     }
   } 
-  Serial.println(" ");
+  Serial.println(" "); // 0rganisation
   delay(5000);
 }
 
---
+++ ESP32evilPortal/README.md
@@ -0,0 +1,1 @@
+S1mple implementation for evil portal attack using the arduino IDE and esp32 spiffs to store html, by its self its harmless, but implementationed with a deauth attack to redirect people into your own EVIL AP 4 credential harvesting is where the magic truly happens.
